30c40f29fc479cab18257e60709e7aa966b91449
Harshmallow <121283182+Abian004@users.noreply.github.com>
2024-09-10 00:11:55 +0700

    Update login-script.js
    Mengganti dan menambahkan comment

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,28 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+
+// Membuat variabel untuk setiap element view 
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+// Membuat variabel untuk menyimpan informasi login 
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
-loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+loginFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Mendapatkan input dari masing-masing value 
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+
+    // Memastikan input kredensial sesuai dengan yang disimpan
+    if (expectedEmail == email && expectedPassword == password) {
+      goToHome(); /* If login successful, go to main page */
+    } else {
+      showPopUp(); /* If login unsuccessful, show popup error */
+    }
 });
